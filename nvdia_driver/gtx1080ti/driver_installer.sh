12469c1126772c34b77558da07d2811a14aec243
Peter Lai <milk4candy@gmail.com.com>
2017-08-19 14:59:23 +0800

    marked nouse code

--- nvdia_driver/gtx1080ti/driver_installer.sh
+++ nvdia_driver/gtx1080ti/driver_installer.sh
@@ -1,4 +1,6 @@
-add-apt-repository ppa:graphics-drivers/ppa
-apt-get update
-apt-get install nvidia-384
+#有裝CUDA就會連DRIVER一起裝
+
+#add-apt-repository ppa:graphics-drivers/ppa
+#apt-get update
+#apt-get install nvidia-384
 
