8e1a66596a716fa36b2c422a5b9716cbdbdbef05
Peter Lai <milk4candy@gmail.com.com>
2017-08-19 19:12:57 +0800

    unmark must run codes back

--- nvdia_driver/gtx1080ti/driver_installer.sh
+++ nvdia_driver/gtx1080ti/driver_installer.sh
@@ -1,6 +1,4 @@
-#有裝CUDA就會連DRIVER一起裝
-
-#add-apt-repository ppa:graphics-drivers/ppa
-#apt-get update
-#apt-get install nvidia-384
+add-apt-repository ppa:graphics-drivers/ppa
+apt-get update
+apt-get install nvidia-384
 
